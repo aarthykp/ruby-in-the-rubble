923321bedacd481c46bb2afc7249aa53971fd232
root <root@201011JUNC0396.(none)>
2015-03-06 15:10:10 +0530

    Array Example

---
+++ array.rb
@@ -0,0 +1,9 @@
+grocey_list = [1,2,3,4,5]
+puts grocey_list
+grocey_list1 = ["marker","parker","colour_pencil"]
+puts grocey_list1
+ad = [4,6,5,5,10]
+puts  var = ad[0]+ad[1]+ad[2]+ad[3]+ad[4]
+value = 0
+gets value[5]
+puts val = value[0]+value[1]+value[2]+value[3]+value[4]
